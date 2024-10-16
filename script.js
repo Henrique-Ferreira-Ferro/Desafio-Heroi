33ebb42850ad4791300ae9def752f6acaa0ddf72
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-10-16 10:17:36 -0300

    Meio caminho andamento com o desafio

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 
+let nome;
+let idade;
+let tipo;
 
+
+class hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+}
+
+const heroi1 = new hero("Hercules", 24, "Guerreiro");
+
+
+atacar(heroi1.tipo, verificaTipo(heroi1.tipo));
+
+console.log(heroi1);
+
+function verificaTipo(tipo){
+    if(tipo === "mago"){
+        return "magia";
+    }else if(tipo === "Guerreiro"){
+        return "espada";
+    }else if(tipo === "Monge"){
+        return "artes marciais";
+    }else if(tipo === "Ninja"){
+        return "shuriken";
+    }else{
+        return "Ops,o tipo não existe :(";
+    }
+}
+
+function atacar(tipo, ataque){
+    console.log("O "+ tipo+" atacou usando "+ ataque);
+}
