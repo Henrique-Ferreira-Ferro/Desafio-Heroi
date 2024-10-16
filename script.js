4e12845f718e4aec36b701c13bfcea518f2de622
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-10-16 10:24:44 -0300

    Implementação do DOM e construção do html

--- script.js
+++ script.js
@@ -1,38 +1,46 @@
 
-let nome;
-let idade;
-let tipo;
 
-
-class hero{
-    constructor(nome, idade, tipo){
+class Hero {
+    constructor(nome, idade, tipo) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
     }
-}
 
-const heroi1 = new hero("Hercules", 24, "Guerreiro");
-
-
-atacar(heroi1.tipo, verificaTipo(heroi1.tipo));
-
-console.log(heroi1);
-
-function verificaTipo(tipo){
-    if(tipo === "mago"){
+ // Método para verificar o tipo e definir o ataque
+ verificaTipo() {
+    if (this.tipo === "mago") {
         return "magia";
-    }else if(tipo === "Guerreiro"){
+    } else if (this.tipo === "Guerreiro") {
         return "espada";
-    }else if(tipo === "Monge"){
+    } else if (this.tipo === "Monge") {
         return "artes marciais";
-    }else if(tipo === "Ninja"){
+    } else if (this.tipo === "Ninja") {
         return "shuriken";
-    }else{
-        return "Ops,o tipo não existe :(";
+    } else {
+        return "Ops, o tipo não existe :(";
     }
 }
 
-function atacar(tipo, ataque){
-    console.log("O "+ tipo+" atacou usando "+ ataque);
+// Método para realizar o ataque
+atacar() {
+    const ataque = this.verificaTipo();
+    return `O ${this.tipo} atacou usando ${ataque}.`;
+}
+}
+
+
+
+
+function criarHeroi() {
+    // Pegando valores do input
+    const nome = document.getElementById('nome').value;
+    const idade = document.getElementById('idade').value;
+    const tipo = document.getElementById('tipo').value;
+
+    // Criando um novo herói com os valores fornecidos
+    const heroi = new Hero(nome, idade, tipo);
+
+    // Mostrando o resultado do ataque no DOM
+    document.getElementById('resultado').innerText = heroi.atacar();
 }
